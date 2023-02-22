5e5b7a7d985be91120cb3913bfe16b1d8d95820a
Anh Huynh <anhb1910030@student.ctu.edu.vn>
2023-02-22 17:46:07 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
     //Middleware xu ly loi tap trung.
     //Trong cac doan code xu ly o cac route, goi next(error)
     //   se chuyen ve middleware xu ly loi nay
